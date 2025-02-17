567af340ed868b8b3084418d837d95d96ef741be
Jayraj <arajjayraj18@gmail.com>
2025-02-17 11:47:39 +0530

    add lenis

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Lenis from "@studio-freight/lenis";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import Skills from "./Components/Skills";
 import About from "./Components/About";
 import Footer from "./Components/Footer";
+
+gsap.registerPlugin(ScrollTrigger);
+
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      smooth: true,
+    });
+
+    lenis.on("scroll", ScrollTrigger.update);
+
+    gsap.ticker.add((time) => {
+      lenis.raf(time * 1000);
+    });
+
+    gsap.ticker.lagSmoothing(0);
+
+    return () => {
+      gsap.ticker.remove(lenis.raf);
+      lenis.destroy();
+    };
+  }, []);
+
   return (
     <>
-      <div className="">
+      <div className="app">
         <Navbar />
-
         <About />
-
         <Skills />
         <Footer />
       </div>
